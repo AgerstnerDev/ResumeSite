0b1e3a77a3370706ba2e99802dad72a020311ac9
ARS-Andrew <94069507+ARS-Andrew@users.noreply.github.com>
2022-04-21 22:04:19 -0500

    Init Commit
    Project initialized and deployed to Firebase hosting

---
+++ my-resume/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Site currently under construction!  Please come back later :)
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/andrew-gerstner/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My Linkedin
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
